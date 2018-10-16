757231d9b78b8f56883d03a23c15669dc53bd292
Schafranek <schafranek@integreat-app.de>
2018-10-16 15:44:21 +0200

    Satz geändert

---
+++ test/test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string s = Console.In.ReadLine();
+            Console.Out.WriteLine("Hallo, " + s + "!");
+        }
+    }
+}
